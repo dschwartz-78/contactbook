d4fc23df11b261d37838ff83b86bfbcd7d4d7c0c
dschwartz-78 <61052906+dschwartz-78@users.noreply.github.com>
2022-04-04 20:52:08 +0200

    Configure project for OVH server

--- contactbook/wsgi.py
+++ contactbook/wsgi.py
@@ -9,6 +9,9 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 
 import os
 
+this_file = "venv/bin/activate_this.py"
+exec(open(this_file).read(), {'__file__': this_file})
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'contactbook.settings')
